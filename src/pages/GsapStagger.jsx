fa02127c34fb111b0efc52ff5db7d4fdb3a6d341
Connie <conniedpaula@gmail.com>
2025-11-04 13:08:13 +0000

    chore: stagger property

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,28 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
-  // TODO: Implement the gsap.stagger() method
+  // Stagger is just a property we can add to any animation
+
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y: 250,
+      rotation: 360,
+      borderRadius: "100%",
+      repeat: -1,
+      yoyo: true,
+      // simple property
+      // stagger: 0.5
+      // complex property: use object
+      stagger: {
+        amount: 1.5,
+        grid: [2,1 ],
+        axis: 'y',
+        ease: 'circ.inOut',
+        from: 'center'
+      }
+    });
+  }, []);
 
   return (
     <main>
